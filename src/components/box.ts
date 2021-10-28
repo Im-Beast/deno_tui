cfa825ebf58675b9d85c076525a7b4421c21f431
Im-Beast <franik.mateusz@gmail.com>
2021-10-28 12:01:15 +0200

    fix dynamic rendering

--- src/components/box.ts
+++ src/components/box.ts
@@ -8,18 +8,18 @@ import { TuiObject } from "../types.ts";
 
 export type CreateBoxOptions = Omit<
   CreateComponentOptions,
-  "interactive" | "name"
+  "interactive" | "name" | "draw"
 >;
 
 export function createBox(object: TuiObject, options: CreateBoxOptions) {
-  const { row, column, width, height } = options.rectangle;
-
   const box = createComponent(object, {
     name: "box",
     interactive: false,
     ...options,
     draw() {
       const styler = getCurrentStyler(box);
+
+      const { row, column, width, height } = options.rectangle;
 
       for (let r = row; r < row + height; ++r) {
         for (let c = column; c < column + width; ++c) {
