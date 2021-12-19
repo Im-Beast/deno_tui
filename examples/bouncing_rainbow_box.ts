17e6aa29010f3915e6c193bdd2d6ca9011110690
Im-Beast <franik.mateusz@gmail.com>
2021-12-19 20:00:17 +0100

    feat: add weird example

---
+++ examples/bouncing_rainbow_box.ts
@@ -0,0 +1,55 @@
+import {
+  compileStyler,
+  createBox,
+  createTui,
+  draw,
+  getStaticValue,
+  hsl,
+  TuiStyler,
+} from "../mod.ts";
+
+const tui = createTui(Deno.stdin, Deno.stdout, {
+  styler: compileStyler<TuiStyler>({
+    background: "black",
+  }),
+});
+
+draw(tui);
+
+let hue = 0;
+const box = createBox(tui, {
+  rectangle: {
+    column: 0,
+    row: 0,
+    height: 5,
+    width: 10,
+  },
+  styler: () => ({
+    background: hsl(++hue % 360, 50, 50, true),
+  }),
+});
+
+const direction = {
+  x: 1,
+  y: 1,
+};
+
+tui.on("draw", () => {
+  const rectangle = getStaticValue(box.rectangle);
+  rectangle.column += direction.x;
+  rectangle.row += direction.y;
+
+  if (
+    rectangle.column < 0 ||
+    (rectangle.column + rectangle.width) > tui.rectangle().width
+  ) {
+    direction.x *= -1;
+  }
+
+  if (
+    rectangle.row < 0 ||
+    (rectangle.row + rectangle.height) > tui.rectangle().height
+  ) {
+    direction.y *= -1;
+  }
+});
