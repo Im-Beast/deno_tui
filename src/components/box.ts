3947d586a869ded730f8dfe3424c64d13c03b6a9
Im-Beast <franik.mateusz@gmail.com>
2022-07-04 20:50:45 +0200

    feat: make neccessary changes to make typings user-friendly, fix: checking height based on row offset

--- src/components/box.ts
+++ src/components/box.ts
@@ -1,10 +1,20 @@
-import { Component } from "../component.ts";
+import { Component, ComponentOptions } from "../component.ts";
 import { Rectangle } from "../types.ts";
+
+export interface BoxComponentOptions extends ComponentOptions {
+  rectangle: Rectangle;
+}
 
 export class BoxComponent extends Component {
   declare rectangle: Rectangle;
 
+  constructor(options: BoxComponentOptions) {
+    super(options);
+  }
+
   draw() {
+    super.draw();
+
     const { canvas } = this.tui;
     const { column, row, width, height } = this.rectangle;
 
@@ -12,7 +22,7 @@ export class BoxComponent extends Component {
 
     const textRow = color(" ".repeat(width));
 
-    for (let r = row; r < height; ++r) {
+    for (let r = row; r < row + height; ++r) {
       canvas.draw(column, r, textRow);
     }
   }
