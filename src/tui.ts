17d9e5c2ce6aec4ab2fc6aabf10a4ff7f6890871
Im-Beast <franik.mateusz@gmail.com>
2022-07-04 20:45:20 +0200

    feat: make updates independent of drawing, add keypress events

--- src/tui.ts
+++ src/tui.ts
@@ -1,15 +1,18 @@
 import { Canvas } from "./canvas.ts";
 import { Component } from "./component.ts";
 import { crayon } from "./deps.ts";
+import { KeyPress, MousePress, MultiKeyPress } from "./key_reader.ts";
 import type { Theme } from "./theme.ts";
 import type { Stdin, Stdout } from "./types.ts";
-import { Timing, TypedCustomEvent, TypedEventTarget } from "./util.ts";
+import { sleep, Timing, TypedCustomEvent, TypedEventTarget } from "./util.ts";
+import { MuxAsyncIterator } from "https://deno.land/std/async/mux_async_iterator.ts";
 
 interface TuiOptions {
   canvas?: Canvas;
   stdin?: Stdin;
   stdout?: Stdout;
   theme?: Partial<Theme>;
+  updateRate?: number;
 }
 
 interface TuiPrivate {
@@ -17,6 +20,7 @@ interface TuiPrivate {
   stdin: Stdin;
   stdout: Stdout;
   components: Component[];
+  updateRate: number;
 }
 
 type TuiImplementation = TuiOptions & TuiPrivate;
@@ -25,25 +29,27 @@ export class Tui extends TypedEventTarget<{
   render: {
     timing: Timing;
   };
-  update: {
-    timing: Timing;
-  };
+  update: void;
+  keyPress: KeyPress;
+  multiKeyPress: MultiKeyPress;
+  mousePress: MousePress;
 }> implements TuiImplementation {
   canvas: Canvas;
   stdin: Stdin;
   stdout: Stdout;
   theme: Theme;
   components: Component[];
+  updateRate: number;
 
-  constructor({ stdin, stdout, canvas, theme }: TuiOptions) {
+  constructor({ stdin, stdout, canvas, theme, updateRate }: TuiOptions) {
     super();
 
     this.stdin = stdin ?? Deno.stdin;
     this.stdout = stdout ?? Deno.stdout;
     this.theme = {
       base: theme?.base ?? crayon,
-      focused: theme?.focused ?? crayon,
-      active: theme?.active ?? crayon,
+      focused: theme?.focused ?? theme?.base ?? crayon,
+      active: theme?.active ?? theme?.focused ?? theme?.base ?? crayon,
     };
     this.components = [];
     this.canvas = canvas ?? new Canvas({
@@ -51,19 +57,46 @@ export class Tui extends TypedEventTarget<{
       refreshRate: 16,
       stdout: Deno.stdout,
     });
+    this.updateRate = updateRate ?? this.canvas.refreshRate;
   }
 
-  async *run() {
+  async *update(): AsyncGenerator<{ type: "update" }> {
+    while (true) {
+      let deltaTime = performance.now();
+
+      this.dispatchEvent(
+        new TypedCustomEvent("update"),
+      );
+      yield { type: "update" };
+
+      deltaTime -= performance.now();
+      await sleep(this.updateRate + (deltaTime / 2));
+    }
+  }
+
+  async *render(): AsyncGenerator<{ type: "render"; timing: Timing }> {
     for await (const timing of this.canvas.render()) {
       this.dispatchEvent(
         new TypedCustomEvent("render", { detail: { timing } }),
       );
-      yield timing;
+      yield { type: "render", timing };
+    }
+  }
 
-      if (timing === Timing.Post) {
-        for (const component of this.components) {
-          component.draw();
-        }
+  async *run(): AsyncGenerator<
+    { type: "render"; timing: Timing } | { type: "update" }
+  > {
+    const iterator = new MuxAsyncIterator<
+      { type: "render"; timing: Timing } | { type: "update" }
+    >();
+    iterator.add(this.render());
+    iterator.add(this.update());
+
+    for await (const event of iterator) {
+      yield event;
+
+      for (const component of this.components) {
+        component.draw();
       }
     }
   }
