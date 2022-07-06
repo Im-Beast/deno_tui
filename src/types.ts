888e53581eb50af361e7415c7648de2fef54ab2a
Im-Beast <franik.mateusz@gmail.com>
2022-07-06 22:34:13 +0200

    feat: add needed types for key_reader.ts

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,13 @@ export interface Rectangle {
 }
 
 export type ConsoleSize = ReturnType<typeof Deno.consoleSize>;
+
+type _Range<From extends number, To extends number, R extends unknown[]> =
+  R["length"] extends To ? To
+    : 
+      | (R["length"] extends Range<0, From> ? From : R["length"])
+      | _Range<From, To, [To, ...R]>;
+
+export type Range<From extends number, To extends number> = number extends From
+  ? number
+  : _Range<From, To, []>;
