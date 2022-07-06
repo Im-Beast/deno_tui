1605bd0a0cb4e85c7ce49fb66f9e461c6b193983
Im-Beast <franik.mateusz@gmail.com>
2022-07-06 22:31:17 +0200

    feat: move tui handling from key_reader.ts to keyboard.ts

---
+++ src/keyboard.ts
@@ -0,0 +1,35 @@
+import { MultiKeyPress, readKeypresses } from "./key_reader.ts";
+import { Tui } from "./tui.ts";
+import { TypedCustomEvent } from "./util.ts";
+
+export async function handleKeypresses(tui: Tui): Promise<void> {
+  for await (const keyPresses of readKeypresses(tui.stdin)) {
+    const multiKeyPress: MultiKeyPress = {
+      keys: [],
+      buffer: [],
+      ctrl: false,
+      meta: false,
+      shift: false,
+    };
+
+    for (const keyPress of keyPresses) {
+      tui.dispatchEvent(
+        keyPress.key === "mouse"
+          ? new TypedCustomEvent("mousePress", { detail: keyPress })
+          : new TypedCustomEvent("keyPress", { detail: keyPress }),
+      );
+
+      multiKeyPress.keys.push(keyPress.key);
+      multiKeyPress.buffer.push(keyPress.buffer);
+      multiKeyPress.shift ||= keyPress.shift;
+      multiKeyPress.meta ||= keyPress.meta;
+      multiKeyPress.ctrl ||= keyPress.ctrl;
+    }
+
+    if (multiKeyPress.keys.length > 1) {
+      tui.dispatchEvent(
+        new TypedCustomEvent("multiKeyPress", { detail: multiKeyPress }),
+      );
+    }
+  }
+}
