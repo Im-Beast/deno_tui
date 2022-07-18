b0623159d42d11ca4f20c41b2733c83b6df5035e
Im-Beast <franik.mateusz@gmail.com>
2022-07-18 18:16:20 +0200

    chore: update test-demo

--- examples/demo.ts
+++ examples/demo.ts
@@ -1,4 +1,6 @@
 import { Canvas } from "../src/canvas.ts";
+import { ButtonComponent } from "../src/components/button.ts";
+import { ScrollableViewComponent } from "../src/components/scrollable_view.ts";
 import { TextboxComponent } from "../src/components/textbox.ts";
 import { crayon } from "../src/deps.ts";
 import { handleKeypresses } from "../src/keyboard.ts";
@@ -33,6 +35,47 @@ new TextboxComponent({
   multiline: true,
 });
 
+const view = new ScrollableViewComponent({
+  tui,
+  rectangle: {
+    column: 20,
+    row: 3,
+    height: 10,
+    width: 20,
+  },
+  theme: {
+    base: crayon.bgBlue,
+    scrollbar: {
+      vertical: {
+        track: crayon.bgYellow,
+        thumb: crayon.bgRed,
+      },
+      horizontal: {
+        track: crayon.bgMagenta,
+        thumb: crayon.bgRed,
+      },
+      corner: crayon.bgLightGreen,
+    },
+  },
+  zIndex: 1,
+});
+
+new ButtonComponent({
+  tui: view.tui,
+  rectangle: {
+    column: 20,
+    row: 10,
+    width: 10,
+    height: 3,
+  },
+  theme: {
+    base: crayon.bgLightGreen,
+    focused: crayon.bgLightCyan,
+    active: crayon.bgLightMagenta,
+  },
+  zIndex: 5,
+});
+
 for await (const _ of tui.run()) {
-  //
+  tui.canvas.draw(0, 0, tui.canvas.fps.toFixed(2));
 }
