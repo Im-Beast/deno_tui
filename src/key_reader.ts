9453f29701faf9351e6ce48922a05bbfe32ee820
Im-Beast <franik.mateusz@gmail.com>
2022-08-01 20:21:18 +0200

    feat: add decoding for more special keys on windows

--- src/key_reader.ts
+++ src/key_reader.ts
@@ -125,13 +125,13 @@ export async function* readKeypresses(stdin: Stdin): AsyncGenerator<(KeyPress | 
         // TODO: Make reading keypresses happen in a Worker
         let char = dll.symbols._getch();
 
-        let special = false;
+        let special: number;
         if (char === 0 || char === 224) {
-          special = true;
+          special = char;
           char = dll.symbols._getch();
         }
 
-        yield [...decodeWindowsChar(char, special)];
+        yield [...decodeWindowsChar(char, special!)];
       }
     }
     default:
@@ -148,8 +148,11 @@ export async function* readKeypresses(stdin: Stdin): AsyncGenerator<(KeyPress | 
   }
 }
 
-export function* decodeWindowsChar(buffer: number, specialKey: boolean): Generator<KeyPress | MousePress, void, void> {
-  console.log(specialKey, buffer);
+export function* decodeWindowsChar(
+  buffer: number,
+  specialCharCode?: number,
+): Generator<KeyPress | MousePress, void, void> {
+  console.log(specialCharCode === 224 || specialCharCode === 0, specialCharCode, buffer);
 
   const decodedBuffer = String.fromCharCode(buffer);
 
@@ -161,9 +164,210 @@ export function* decodeWindowsChar(buffer: number, specialKey: boolean): Generat
     ctrl: false,
   };
 
-  if (specialKey) {
-    // TODO: Add more special keys
+  if (typeof specialCharCode !== "undefined") {
     switch (buffer) {
+      case 13:
+        keyPress.key = "return";
+        break;
+      case 32:
+        keyPress.key = "space";
+        break;
+      case 9:
+        keyPress.key = "tab";
+        break;
+      case 27:
+        keyPress.key = "escape";
+        break;
+      case 8:
+        keyPress.key = "backspace";
+        break;
+
+      // F1-F10 = 59-68
+      // shift:   +25
+      // ctrl:    +35
+      // meta:    +45
+
+      case 59:
+        keyPress.key = "f1";
+        break;
+      case 59 + 25:
+        keyPress.key = "f1";
+        keyPress.shift = true;
+        break;
+      case 59 + 35:
+        keyPress.key = "f1";
+        keyPress.ctrl = true;
+        break;
+      case 59 + 45:
+        keyPress.key = "f1";
+        keyPress.meta = true;
+        break;
+      case 60:
+        keyPress.key = "f2";
+        break;
+      case 60 + 25:
+        keyPress.key = "f2";
+        keyPress.shift = true;
+        break;
+      case 60 + 35:
+        keyPress.key = "f2";
+        keyPress.ctrl = true;
+        break;
+      case 60 + 45:
+        keyPress.key = "f2";
+        keyPress.meta = true;
+        break;
+      case 61:
+        keyPress.key = "f3";
+        break;
+      case 61 + 25:
+        keyPress.key = "f3";
+        keyPress.shift = true;
+        break;
+      case 61 + 35:
+        keyPress.key = "f3";
+        keyPress.ctrl = true;
+        break;
+      case 61 + 45:
+        keyPress.key = "f3";
+        keyPress.meta = true;
+        break;
+      case 62:
+        keyPress.key = "f4";
+        break;
+      case 62 + 25:
+        keyPress.key = "f4";
+        keyPress.shift = true;
+        break;
+      case 62 + 35:
+        keyPress.key = "f4";
+        keyPress.ctrl = true;
+        break;
+      case 62 + 45:
+        keyPress.key = "f4";
+        keyPress.meta = true;
+        break;
+      case 63:
+        keyPress.key = "f5";
+        break;
+      case 63 + 25:
+        keyPress.key = "f5";
+        keyPress.shift = true;
+        break;
+      case 63 + 35:
+        keyPress.key = "f5";
+        keyPress.ctrl = true;
+        break;
+      case 63 + 45:
+        keyPress.key = "f5";
+        keyPress.meta = true;
+        break;
+      case 64:
+        keyPress.key = "f6";
+        break;
+      case 64 + 25:
+        keyPress.key = "f6";
+        keyPress.shift = true;
+        break;
+      case 64 + 35:
+        keyPress.key = "f6";
+        keyPress.ctrl = true;
+        break;
+      case 65:
+        keyPress.key = "f7";
+        break;
+      case 65 + 25:
+        keyPress.key = "f7";
+        keyPress.shift = true;
+        break;
+      case 65 + 35:
+        keyPress.key = "f7";
+        keyPress.ctrl = true;
+        break;
+      case 65 + 45:
+        keyPress.key = "f7";
+        keyPress.meta = true;
+        break;
+      case 66:
+        keyPress.key = "f8";
+        break;
+      case 66 + 25:
+        keyPress.key = "f8";
+        keyPress.shift = true;
+        break;
+      case 66 + 35:
+        keyPress.key = "f8";
+        keyPress.ctrl = true;
+        break;
+      case 66 + 45:
+        keyPress.key = "f8";
+        keyPress.meta = true;
+        break;
+      case 67:
+        keyPress.key = "f9";
+        break;
+      case 67 + 25:
+        keyPress.key = "f9";
+        keyPress.shift = true;
+        break;
+      case 67 + 35:
+        keyPress.key = "f9";
+        keyPress.ctrl = true;
+        break;
+      case 67 + 45:
+        keyPress.key = "f9";
+        keyPress.meta = true;
+        break;
+      case 68:
+        keyPress.key = "f10";
+        break;
+      case 68 + 25:
+        keyPress.key = "f10";
+        keyPress.shift = true;
+        break;
+      case 68 + 35:
+        keyPress.key = "f10";
+        keyPress.ctrl = true;
+        break;
+      case 68 + 45:
+        keyPress.key = "f10";
+        keyPress.meta = true;
+        break;
+      // F11-F12 = 133-134
+      // shift:    +2
+      // ctrl:     +4
+      // meta:     +6
+      case 133:
+        keyPress.key = "f11";
+        break;
+      case 135:
+        keyPress.key = "f11";
+        keyPress.shift = true;
+        break;
+      case 137:
+        keyPress.key = "f11";
+        keyPress.ctrl = true;
+        break;
+      case 139:
+        keyPress.key = "f11";
+        keyPress.meta = true;
+        break;
+      case 134:
+        keyPress.key = "f12";
+        break;
+      case 136:
+        keyPress.key = "f12";
+        keyPress.shift = true;
+        break;
+      case 138:
+        keyPress.key = "f12";
+        keyPress.ctrl = true;
+        break;
+      case 140:
+        keyPress.key = "f12";
+        keyPress.meta = true;
+        break;
+
       case 72:
         keyPress.key = "up";
         break;
@@ -175,6 +379,7 @@ export function* decodeWindowsChar(buffer: number, specialKey: boolean): Generat
         keyPress.key = "up";
         keyPress.meta = true;
         break;
+
       case 80:
         keyPress.key = "down";
         break;
@@ -186,6 +391,7 @@ export function* decodeWindowsChar(buffer: number, specialKey: boolean): Generat
         keyPress.key = "down";
         keyPress.meta = true;
         break;
+
       case 75:
         keyPress.key = "left";
         break;
@@ -197,6 +403,7 @@ export function* decodeWindowsChar(buffer: number, specialKey: boolean): Generat
         keyPress.key = "left";
         keyPress.meta = true;
         break;
+
       case 77:
         keyPress.key = "right";
         break;
@@ -206,6 +413,79 @@ export function* decodeWindowsChar(buffer: number, specialKey: boolean): Generat
         break;
       case 157:
         keyPress.key = "right";
+        keyPress.meta = true;
+        break;
+
+      case 82:
+        keyPress.key = "insert";
+        break;
+      case 146:
+        keyPress.key = "insert";
+        keyPress.ctrl = true;
+        break;
+      case 162:
+        keyPress.key = "insert";
+        keyPress.meta = true;
+        break;
+
+      case 83:
+        keyPress.key = "delete";
+        break;
+      case 147:
+        keyPress.key = "delete";
+        keyPress.ctrl = true;
+        break;
+      case 163:
+        keyPress.key = "delete";
+        keyPress.meta = true;
+        break;
+
+      case 71:
+        keyPress.key = "home";
+        break;
+      case 119:
+        keyPress.key = "home";
+        keyPress.ctrl = true;
+        break;
+      case 151:
+        keyPress.key = "home";
+        keyPress.meta = true;
+        break;
+
+      case 79:
+        keyPress.key = "end";
+        break;
+      case 117:
+        keyPress.key = "end";
+        keyPress.ctrl = true;
+        break;
+      case 159:
+        keyPress.key = "end";
+        keyPress.meta = true;
+        break;
+
+      case 73:
+        keyPress.key = "pageup";
+        break;
+      // Windows is so dumb, F12 is the same as pageup with ctrl held
+      // case 134:
+      //   keyPress.key = "pageup";
+      //   keyPress.ctrl = true;
+      //   break;
+      case 153:
+        keyPress.key = "pageup";
+        keyPress.meta = true;
+        break;
+
+      case 81:
+        keyPress.key = "pagedown";
+        break;
+      case 118:
+        keyPress.key = "pagedown";
+        keyPress.ctrl = true;
+        break;
+      case 161:
+        keyPress.key = "pagedown";
         keyPress.meta = true;
         break;
     }
