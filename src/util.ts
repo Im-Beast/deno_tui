0ff2fad9b199a43da9868b0ccb33f6ef90866723
Im-Beast <franik.mateusz@gmail.com>
2022-08-07 16:32:02 +0200

    feat: add isFullWidth and textWidth functions

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
+import { crayon } from "./deps.ts";
+import { Rectangle } from "./types.ts";
+
 export enum Timing {
   Pre = "pre",
   Post = "post",
@@ -22,12 +25,53 @@ export function fits(number: number, min: number, max: number): boolean {
   return number === clamp(number, min, max);
 }
 
+export function fitsInRectangle(column: number, row: number, rectangle?: Rectangle): boolean {
+  if (!rectangle) return true;
+  return fits(column, rectangle.column, rectangle.column + rectangle.width) &&
+    fits(row, rectangle.row, rectangle.row + rectangle.height);
+}
+
 export function insertAt(string: string, index: number, value: string): string {
   return string.slice(0, index) + value + string.slice(index);
 }
 
 export function normalize(value: number, min: number, max: number): number {
   return ((value - min) / (max - min)) || 0;
+}
+
+export function textWidth(text: string): number {
+  if (!text) return 0;
+
+  let width = 0;
+  for (const letter of crayon.strip(text)) {
+    width += isFullWidth(letter) ? 2 : 1;
+  }
+
+  return width;
+}
+
+export function isFullWidth(character: string): boolean {
+  const codePoint = character.charCodeAt(0);
+
+  return (
+    codePoint >= 0x1100 &&
+    (codePoint <= 0x115f ||
+      codePoint === 0x2329 ||
+      codePoint === 0x232a ||
+      (0x2e80 <= codePoint && codePoint <= 0x3247 && codePoint !== 0x303f) ||
+      (0x3250 <= codePoint && codePoint <= 0x4dbf) ||
+      (0x4e00 <= codePoint && codePoint <= 0xa4c6) ||
+      (0xa960 <= codePoint && codePoint <= 0xa97c) ||
+      (0xac00 <= codePoint && codePoint <= 0xd7a3) ||
+      (0xf900 <= codePoint && codePoint <= 0xfaff) ||
+      (0xfe10 <= codePoint && codePoint <= 0xfe19) ||
+      (0xfe30 <= codePoint && codePoint <= 0xfe6b) ||
+      (0xff01 <= codePoint && codePoint <= 0xff60) ||
+      (0xffe0 <= codePoint && codePoint <= 0xffe6) ||
+      (0x1b000 <= codePoint && codePoint <= 0x1b001) ||
+      (0x1f200 <= codePoint && codePoint <= 0x1f251) ||
+      (0x20000 <= codePoint && codePoint <= 0x3fffd))
+  );
 }
 
 export type EventRecord = Record<string, Event>;
