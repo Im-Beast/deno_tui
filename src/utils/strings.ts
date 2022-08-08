3ad55dcd5b0366626392c75be34093329427035f
Im-Beast <franik.mateusz@gmail.com>
2022-08-08 13:05:14 +0200

    feat: add stripStyle function

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,7 +1,10 @@
-import { crayon } from "../deps.ts";
-
 export const UNICODE_CHAR_REGEXP =
   /\ud83c[\udffb-\udfff](?=\ud83c[\udffb-\udfff])|(?:(?:\ud83c\udff4\udb40\udc67\udb40\udc62\udb40(?:\udc65|\udc73|\udc77)\udb40(?:\udc6e|\udc63|\udc6c)\udb40(?:\udc67|\udc74|\udc73)\udb40\udc7f)|[^\ud800-\udfff][\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff\u1ab0-\u1aff\u1dc0-\u1dff]?|[\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff\u1ab0-\u1aff\u1dc0-\u1dff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\ud800-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff\u1ab0-\u1aff\u1dc0-\u1dff]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff\u1ab0-\u1aff\u1dc0-\u1dff]|\ud83c[\udffb-\udfff])?)*/g;
+
+export function stripStyles(string: string): string {
+  // deno-lint-ignore no-control-regex
+  return string.replaceAll(/\x1b\[([0-9]|;)+m/gi, "");
+}
 
 export function insertAt(string: string, index: number, value: string): string {
   return string.slice(0, index) + value + string.slice(index);
@@ -11,7 +14,7 @@ export function textWidth(text: string): number {
   if (!text) return 0;
 
   let width = 0;
-  for (const letter of crayon.strip(text)) {
+  for (const letter of stripStyles(text)) {
     width += isFullWidth(letter) ? 2 : 1;
   }
 
