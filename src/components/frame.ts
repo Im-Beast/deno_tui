eac30df1531f99b71bd761be8cace1e5eeb31db4
Im-Beast <franik.mateusz@gmail.com>
2022-08-10 21:50:19 +0200

    chore: fix typings, calculations, fix: update rectangle on each redraw

--- src/components/frame.ts
+++ src/components/frame.ts
@@ -1,8 +1,6 @@
 // Copyright 2022 Im-Beast. All rights reserved. MIT license.
 
-import { Component } from "../component.ts";
-import { Theme } from "../theme.ts";
-import { Tui } from "../tui.ts";
+import { Component, ComponentOptions } from "../component.ts";
 import { Rectangle } from "../types.ts";
 import { EventRecord } from "../utils/typed_event_target.ts";
 
@@ -29,9 +27,8 @@ export type FramePieceType = {
 };
 
 export type FrameComponentOptions =
+  & ComponentOptions
   & {
-    tui: Tui;
-    theme?: Partial<Theme>;
     framePieces?: "sharp" | "rounded" | FramePieceType;
   }
   & (
@@ -42,17 +39,31 @@ export class FrameComponent<EventMap extends EventRecord = Record<never, never>>
   component?: Component;
   framePieces: "sharp" | "rounded" | FramePieceType;
 
-  constructor(
-    { tui, component, rectangle, theme, framePieces }: FrameComponentOptions,
-  ) {
+  #initialRectangle?: Rectangle;
+
+  constructor({ tui, view, component, rectangle, theme, framePieces, zIndex }: FrameComponentOptions) {
     super({
       tui,
+      view,
       rectangle,
       theme: theme ?? component?.theme,
-      zIndex: component?.zIndex,
+      zIndex: zIndex ?? component?.zIndex,
     });
     this.component = component;
     this.framePieces = framePieces ?? "sharp";
+
+    this.#initialRectangle = this.rectangle;
+    this.rectangle = (this.rectangle ?? this.component?.rectangle)!;
+    this.rectangle = {
+      column: this.rectangle.column - 1,
+      row: this.rectangle.row - 1,
+      width: this.rectangle.width + 2,
+      height: this.rectangle.height + 2,
+    };
+
+    if (!this.rectangle && !this.component?.rectangle) {
+      throw new Error("You need to pass either rectangle or component that has rectangle to FrameComponent");
+    }
   }
 
   set state(_value) {}
@@ -65,13 +76,16 @@ export class FrameComponent<EventMap extends EventRecord = Record<never, never>>
     super.draw();
 
     const { style, framePieces } = this;
-    const { canvas } = this.tui;
-    let { column, row, width, height } = (this.rectangle ?? this.component?.rectangle)!;
+    const { canvas } = this.component?.tui ?? this.tui;
 
+    this.rectangle = this.#initialRectangle;
+    this.rectangle = (this.rectangle ?? this.component?.rectangle)!;
+    let { column, row, width, height } = this.rectangle;
     column -= 1;
     row -= 1;
-    width += 1;
-    height += 1;
+    width += 2;
+    height += 2;
+    this.rectangle = { column, row, width, height };
 
     const pieces = framePieces === "sharp"
       ? sharpFramePieces
@@ -80,18 +94,19 @@ export class FrameComponent<EventMap extends EventRecord = Record<never, never>>
       : framePieces;
 
     canvas.draw(column, row, style(pieces.topLeft));
-    canvas.draw(column + width, row, style(pieces.topRight));
-    canvas.draw(column, row + height, style(pieces.bottomLeft));
-    canvas.draw(column + width, row + height, style(pieces.bottomRight));
+    canvas.draw(column + width - 1, row, style(pieces.topRight));
 
-    for (let x = column + 1; x < column + width; ++x) {
-      canvas.draw(x, row, style(pieces.horizontal));
-      canvas.draw(x, row + height, style(pieces.horizontal));
-    }
-
-    for (let y = row + 1; y < row + height; ++y) {
+    for (let y = row + 1; y < row + height - 1; ++y) {
       canvas.draw(column, y, style(pieces.vertical));
-      canvas.draw(column + width, y, style(pieces.vertical));
+      canvas.draw(column + width - 1, y, style(pieces.vertical));
     }
+
+    for (let x = column + 1; x < column + width - 1; ++x) {
+      canvas.draw(x, row, style(pieces.horizontal));
+      canvas.draw(x, row + height - 1, style(pieces.horizontal));
+    }
+
+    canvas.draw(column, row + height - 1, style(pieces.bottomLeft));
+    canvas.draw(column + width - 1, row + height - 1, style(pieces.bottomRight));
   }
 }
