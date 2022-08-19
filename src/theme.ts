4976f003dbda5232f3081dc07a7b9066f8a0b149
Im-Beast <franik.mateusz@gmail.com>
2022-08-19 15:08:39 +0200

    feat: add hierarchizeTheme() method

--- src/theme.ts
+++ src/theme.ts
@@ -8,6 +8,37 @@ export function emptyStyle(text: string): string {
   return text;
 }
 
+/** Applies default values to properties (lower one hierarchy or `emptyStyle`) that aren't set */
+export function hierarchizeTheme<
+  T extends Theme = Theme,
+  Y extends Partial<T> = Partial<T>,
+>(input: Y): T {
+  let { base, active, focused } = input;
+
+  base ??= emptyStyle;
+  focused ??= base;
+  active ??= focused ?? base;
+
+  const obj: Record<string, unknown> = {};
+
+  const otherKeys = Object
+    .getOwnPropertyNames(input)
+    .filter((key) => !["base", "focused", "active"].includes(key));
+
+  for (const key of otherKeys) {
+    const property = input[key as keyof Y];
+    if (typeof property !== "object") {
+      obj[key] ??= emptyStyle;
+      continue;
+    }
+
+    // deno-lint-ignore no-explicit-any
+    obj[key] = hierarchizeTheme<any, any>(property);
+  }
+
+  return { ...obj, base, focused, active } as T;
+}
+
 /** Base theme used to style components, can be expanded upon */
 export interface Theme {
   /** Default style */
