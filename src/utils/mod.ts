4ca211cf391cb612f1c058635bda8f5bd8834c6a
Im-Beast <franik.mateusz@gmail.com>
2022-09-09 20:22:26 +0200

    chore: update mod.ts

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,7 +1,7 @@
 // Copyright 2022 Im-Beast. All rights reserved. MIT license.
 export * from "./ansi_codes.ts";
 export * from "./async.ts";
-export * from "./combined_async_iterator.ts";
+export * from "./deffered.ts";
 export * from "./numbers.ts";
 export * from "./sorted_array.ts";
 export * from "./strings.ts";
