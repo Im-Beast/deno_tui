37291424426c774b6ce15b093034414041a895bd
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-11-12 14:44:56 -0500

    feat: Added Offset To Draggable Demo

--- examples/draggable_box.ts
+++ examples/draggable_box.ts
@@ -1,95 +1,106 @@
 // Copyright 2022 Im-Beast. All rights reserved. MIT license.
 // Example of creating your own component by extending provided ones
 
-import { crayon } from "https://deno.land/x/crayon@3.3.2/mod.ts";
+import { BoxComponent } from '../src/components/box.ts'
+import { crayon } from 'https://deno.land/x/crayon@3.3.2/mod.ts'
 
-import { handleKeypresses, handleMouseControls, PlaceComponentOptions, Tui } from "../mod.ts";
-import { BoxComponent } from "../src/components/box.ts";
+import { 
+    handleKeypresses , handleMouseControls , 
+    PlaceComponentOptions , Tui 
+} from '../mod.ts'
 
-const tui = new Tui({
-  style: crayon.bgBlack.white,
-});
+
+
+const tui = new Tui({ style : crayon.bgBlack.white });
 
 tui.dispatch();
 
 handleKeypresses(tui);
 handleMouseControls(tui);
 
+
 class DraggableBoxComponent extends BoxComponent {
-  constructor(options: PlaceComponentOptions) {
-    super(options);
+    
+    offset : number []
+    
+    constructor ( options : PlaceComponentOptions ){
+        
+        super(options);
 
-    tui.on("mousePress", ({ drag, x, y }) => {
-      if (!drag || this.state === "base") return;
+        tui.on('mousePress',({ drag , x , y }) => {
+        
+            if(this.state === 'base')
+                return
+        
+            const { rectangle , offset } = this;
+            
+            if(drag){
+                rectangle.column = x - offset.x;
+                rectangle.row = y - offset.y;
+                return;
+            }
 
-      this.rectangle.column = x;
-      this.rectangle.row = y;
-    });
-  }
-
-  // Make component interactable
-  interact(): void {
-    this.state = "focused";
-  }
+            this.offset = {
+                x : x - rectangle.column ,
+                y : y - rectangle.row
+            }
+        });
+    }
+    
+    // Make component interactable
+    
+    interact() : void {
+        this.state = 'focused';
+    }
 }
 
-new DraggableBoxComponent({
-  tui,
-  theme: {
-    base: crayon.bgBlue,
-    focused: crayon.bgLightBlue,
-  },
-  rectangle: {
-    column: 1,
-    row: 1,
-    width: 6,
-    height: 3,
-  },
-});
 
-new DraggableBoxComponent({
-  tui,
-  theme: {
-    base: crayon.bgYellow,
-    focused: crayon.bgLightYellow,
-  },
-  rectangle: {
-    column: 1,
-    row: 1,
-    width: 6,
-    height: 3,
-  },
-  zIndex: 1,
-});
+function spawnBox ({ place , color }){
+    
+    const 
+        [ base , focused ] = color ,
+        [ x , y , z ] = place ;
+    
+    new DraggableBoxComponent({ tui ,
+        
+        zIndex : z ,
+        
+        theme : {
+            focused : crayon[focused] , 
+            base : crayon[base]
+        },
+        
+        rectangle : {
+            
+            height : 3 ,
+            width : 6 ,
+            
+            column : x ,
+            row : y
+        }
+    });
+}
 
-new DraggableBoxComponent({
-  tui,
-  theme: {
-    base: crayon.bgMagenta,
-    focused: crayon.bgLightMagenta,
-  },
-  rectangle: {
-    column: 1,
-    row: 1,
-    width: 6,
-    height: 3,
-  },
-  zIndex: 2,
-});
 
-new DraggableBoxComponent({
-  tui,
-  theme: {
-    base: crayon.bgGreen,
-    focused: crayon.bgLightGreen,
-  },
-  rectangle: {
-    column: 1,
-    row: 1,
-    width: 6,
-    height: 3,
-  },
-  zIndex: 3,
-});
+spawnBox({
+    color : [ 'bgBlue' , 'bgLightBlue' ] ,
+    place : [ 1 , 1 , 0 ]
+})
+
+spawnBox({
+    color : [ 'bgYellow' , 'bgLightYellow' ] ,
+    place : [ 1 , 1 , 1 ]
+})
+
+spawnBox({
+    color : [ 'bgMagenta' , 'bgLightMagenta' ] ,
+    place : [ 1 , 1 , 2 ]
+})
+
+spawnBox({
+    color : [ 'bgGreen' , 'bgLightGreen' ] ,
+    place : [ 1 , 1 , 3 ]
+})
+
 
 tui.run();
