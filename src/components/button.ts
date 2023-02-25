2751f4923436bf3d50324783a61fc1685d31ba20
Im-Beast <franik.mateusz@gmail.com>
2023-02-25 22:18:33 +0100

    feat: add label property to button

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,6 +1,77 @@
+import { ComponentOptions } from "../component.ts";
 import { Box } from "./box.ts";
 
+import type { BoxObject } from "../canvas/box.ts";
+import { Label, LabelAlign } from "./label.ts";
+
+export interface ButtonOptions extends ComponentOptions {
+  label?: {
+    value: string;
+    align?: LabelAlign;
+  };
+}
+
 export class Button extends Box {
+  declare drawnObjects: { box: BoxObject };
+  declare subComponents: { label?: Label };
+  label?: {
+    value: string;
+    align?: LabelAlign;
+  };
+
+  constructor(options: ButtonOptions) {
+    super(options);
+    this.label = options.label;
+  }
+
+  update(): void {
+    super.update();
+
+    const { label } = this.subComponents;
+
+    if (this.label && !label) {
+      this.draw();
+    } else if (label) {
+      if (!this.label) {
+        label.remove();
+        delete this.subComponents.label;
+        return;
+      }
+
+      label.state = this.state;
+      label.theme = this.theme;
+      label.value = this.label.value;
+      label.zIndex = this.zIndex;
+      label.rectangle = this.rectangle;
+
+      if (this.label.align) {
+        label.align = this.label.align;
+      }
+    }
+  }
+
+  draw(): void {
+    super.draw();
+
+    if (this.label) {
+      const label = new Label({
+        parent: this,
+        theme: this.theme,
+        value: this.label.value,
+        zIndex: this.zIndex,
+        rectangle: this.rectangle,
+        align: this.label.align ?? {
+          horizontal: "center",
+          vertical: "center",
+        },
+      });
+
+      // label.subComponentOf = this;
+      this.subComponents.label = label;
+      this.children.push(label);
+    }
+  }
+
   interact(method: "mouse" | "keyboard"): void {
     const interactionInterval = Date.now() - this.lastInteraction.time;
 
