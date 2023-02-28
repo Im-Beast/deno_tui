546ac8345741f9d3d4d83b240f04e6b7e01dfda6
Im-Beast <franik.mateusz@gmail.com>
2023-02-28 15:55:16 +0100

    feat: create Combobox component

---
+++ src/components/combobox.ts
@@ -0,0 +1,86 @@
+import { Button, ButtonOptions } from "./button.ts";
+
+export interface ComboBoxOptions<Items extends string[] = string[]> extends ButtonOptions {
+  items: Items;
+  value?: Items[number];
+}
+
+export class ComboBox<Items extends string[] = string[]> extends Button {
+  // declare drawnObjects: { box: BoxObject };
+  declare subComponents: { [button: number]: Button };
+
+  #visibleItems: boolean;
+
+  items: Items;
+  value?: Items[number];
+  label: { value: string };
+
+  constructor(options: ComboBoxOptions<Items>) {
+    super(options);
+    this.#visibleItems = false;
+    this.value = options.value;
+    this.items = options.items;
+    this.label = options.label ?? {
+      value: options.value ?? options.items[0],
+    };
+  }
+
+  update(): void {
+    super.update();
+
+    const { subComponents, theme, zIndex } = this;
+    const { column, row, width, height } = this.rectangle;
+
+    for (const [i, item] of this.items.entries()) {
+      const subComponent = subComponents[i];
+      if (subComponent) {
+        subComponent.label!.value = item;
+        subComponent.theme = this.theme;
+        subComponent.zIndex = this.zIndex;
+        subComponent.visible = this.#visibleItems;
+
+        subComponent.rectangle.column = column;
+        subComponent.rectangle.row = row + (i + 1) * height;
+        subComponent.rectangle.width = width;
+        subComponent.rectangle.height = height;
+      } else {
+        const button = new Button({
+          parent: this,
+          label: {
+            value: item,
+            align: {
+              horizontal: "center",
+              vertical: "center",
+            },
+          },
+          rectangle: {
+            column,
+            row: row + (i + 1) * height,
+            width,
+            height,
+          },
+          theme,
+          zIndex,
+        });
+
+        button.on("stateChange", () => {
+          if (button.state !== "active") return;
+          this.value = item;
+          this.label.value = item;
+          this.#visibleItems = false;
+          this.emit("valueChange", this);
+        });
+
+        subComponents[i] = button;
+      }
+    }
+  }
+
+  interact(method: "mouse" | "keyboard"): void {
+    super.interact(method);
+
+    if (this.state === "active") {
+      this.#visibleItems = !this.#visibleItems;
+    }
+  }
+}
