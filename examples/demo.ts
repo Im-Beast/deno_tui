4e6ed697dd780fc21cc41789b606c65c81f65280
Im-Beast <franik.mateusz@gmail.com>
2023-03-11 19:56:56 +0100

    chore: update demo to test progressbars

--- examples/demo.ts
+++ examples/demo.ts
@@ -8,6 +8,7 @@ import { handleInput } from "../src/input.ts";
 import { Tui } from "../src/tui.ts";
 import { CheckBox } from "../src/components/checkbox.ts";
 import { ComboBox } from "../src/components/combobox.ts";
+import { ProgressBar } from "../src/components/progressbar.ts";
 
 const tui = new Tui({
   style: crayon.bgBlack,
@@ -186,6 +187,182 @@ new ComboBox({
   zIndex: 5,
 });
 
+const progressBar1 = new ProgressBar({
+  parent: tui,
+  orientation: "horizontal",
+  direction: "normal",
+  rectangle: {
+    column: 74,
+    row: 1,
+    height: 2,
+    width: 10,
+  },
+  smooth: false,
+  min: 0,
+  max: 1,
+  value: 0.5,
+  theme: {
+    base: crayon.bgYellow.green,
+    progress: {
+      base: crayon.bgGreen.yellow,
+    },
+  },
+});
+
+const progressBar2 = new ProgressBar({
+  parent: tui,
+  orientation: "horizontal",
+  direction: "reversed",
+  rectangle: {
+    column: 74,
+    row: 4,
+    height: 2,
+    width: 10,
+  },
+  smooth: false,
+  min: 0,
+  max: 1,
+  value: 0.5,
+  theme: {
+    base: crayon.bgYellow.green,
+    progress: {
+      base: crayon.bgGreen.yellow,
+    },
+  },
+});
+
+const progressBar3 = new ProgressBar({
+  parent: tui,
+  orientation: "horizontal",
+  direction: "normal",
+  rectangle: {
+    column: 74,
+    row: 7,
+    height: 2,
+    width: 10,
+  },
+  smooth: true,
+  min: 0,
+  max: 1,
+  value: 0.5,
+  theme: {
+    base: crayon.bgYellow.green,
+    progress: {
+      base: crayon.bgYellow.green,
+    },
+  },
+});
+
+const progressBar4 = new ProgressBar({
+  parent: tui,
+  orientation: "horizontal",
+  direction: "reversed",
+  rectangle: {
+    column: 74,
+    row: 10,
+    height: 2,
+    width: 10,
+  },
+  smooth: true,
+  min: 0,
+  max: 1,
+  value: 0.5,
+  theme: {
+    base: crayon.bgYellow.green,
+    progress: {
+      base: crayon.bgYellow.green,
+    },
+  },
+});
+
+const progressBar5 = new ProgressBar({
+  parent: tui,
+  orientation: "vertical",
+  direction: "normal",
+  rectangle: {
+    column: 86,
+    row: 1,
+    height: 8,
+    width: 3,
+  },
+  smooth: false,
+  min: 0,
+  max: 1,
+  value: 0.5,
+  theme: {
+    base: crayon.bgYellow.green,
+    progress: {
+      base: crayon.bgGreen.yellow,
+    },
+  },
+});
+
+const progressBar6 = new ProgressBar({
+  parent: tui,
+  orientation: "vertical",
+  direction: "reversed",
+  rectangle: {
+    column: 91,
+    row: 1,
+    height: 8,
+    width: 3,
+  },
+  smooth: false,
+  min: 0,
+  max: 1,
+  value: 0.5,
+  theme: {
+    base: crayon.bgYellow.green,
+    progress: {
+      base: crayon.bgGreen.yellow,
+    },
+  },
+});
+
+const progressBar7 = new ProgressBar({
+  parent: tui,
+  orientation: "vertical",
+  direction: "normal",
+  rectangle: {
+    column: 96,
+    row: 1,
+    height: 8,
+    width: 3,
+  },
+  smooth: true,
+  min: 0,
+  max: 1,
+  value: 0.5,
+  theme: {
+    base: crayon.bgYellow.green,
+    progress: {
+      base: crayon.bgYellow.green,
+    },
+  },
+});
+
+const progressBar8 = new ProgressBar({
+  parent: tui,
+  orientation: "vertical",
+  direction: "reversed",
+  rectangle: {
+    column: 101,
+    row: 1,
+    height: 8,
+    width: 3,
+  },
+  smooth: true,
+  min: 0,
+  max: 1,
+  value: 0.5,
+  theme: {
+    base: crayon.bgYellow.green,
+    progress: {
+      base: crayon.bgYellow.green,
+    },
+  },
+});
+
 tui.on("mousePress", (mousePress) => {
   if (!mousePress.drag || button.state === "base") return;
 
@@ -199,8 +376,22 @@ tui.canvas.on("render", () => {
 });
 
 let i = 0;
+let dir = 0.025;
 
 setInterval(() => {
   label.value = `hello\nworld!\nhow are you?\n${`${i % 10} lines\n`.repeat(i++ % 10)}`;
   text.rectangle.column++;
-}, 100);
+
+  progressBar8.value += dir;
+
+  progressBar1.value =
+    progressBar2.value =
+    progressBar3.value =
+    progressBar4.value =
+    progressBar5.value =
+    progressBar6.value =
+    progressBar7.value =
+      progressBar8.value;
+
+  if (progressBar1.value >= 1 || progressBar1.value <= 0) dir *= -1;
+}, 80);
