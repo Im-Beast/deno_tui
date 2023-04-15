cc8f875785e11365eeda7d48cff3b1def406b2b9
Im-Beast <franik.mateusz@gmail.com>
2023-04-16 00:01:05 +0200

    chore(wip): update demo

--- examples/demo.ts
+++ examples/demo.ts
@@ -1,517 +1,515 @@
-import { crayon } from "https://deno.land/x/crayon@3.3.3/mod.ts";
-import { Box } from "../src/components/box.ts";
-import { Button } from "../src/components/button.ts";
-import { Text } from "../src/components/text.ts";
-import { Label } from "../src/components/label.ts";
-import { handleKeyboardControls, handleMouseControls } from "../src/controls.ts";
-import { handleInput } from "../src/input.ts";
 import { Tui } from "../src/tui.ts";
+import { handleInput } from "../src/input.ts";
+import { handleKeyboardControls, handleMouseControls } from "../src/controls.ts";
+
+import { Box } from "../src/components/box.ts";
+import { Text } from "../src/components/text.ts";
+import { Frame } from "../src/components/frame.ts";
+import { Input } from "../src/components/input.ts";
+import { Label } from "../src/components/label.ts";
+import { Table } from "../src/components/table.ts";
+import { Button } from "../src/components/button.ts";
+import { Slider } from "../src/components/slider.ts";
 import { CheckBox } from "../src/components/checkbox.ts";
 import { ComboBox } from "../src/components/combobox.ts";
 import { ProgressBar } from "../src/components/progressbar.ts";
-import { Slider } from "../src/components/slider.ts";
-import { Frame } from "../src/components/frame.ts";
-import { Input } from "../src/components/input.ts";
+
+import { crayon } from "https://deno.land/x/crayon@3.3.3/mod.ts";
+import { Theme } from "../src/theme.ts";
+import { View } from "../src/view.ts";
+import { clamp, Component, isInteractable } from "../mod.ts";
 
 const tui = new Tui({
   style: crayon.bgBlack,
 });
 
 handleInput(tui);
-handleKeyboardControls(tui);
 handleMouseControls(tui);
+handleKeyboardControls(tui);
 tui.dispatch();
 tui.run();
 
+const baseTheme: Theme = {
+  base: crayon.bgLightBlue,
+  focused: crayon.bgCyan,
+  active: crayon.bgBlue,
+  disabled: crayon.bgLightBlack.black,
+};
+
+const performanceStats = new Text({
+  parent: tui,
+  rectangle: { column: 0, row: 0 },
+  theme: baseTheme,
+  value: "",
+});
+
 new Box({
   parent: tui,
+  theme: baseTheme,
   rectangle: {
     column: 2,
-    row: 1,
-    width: 10,
+    row: 3,
     height: 5,
-  },
-  theme: {
-    base: crayon.bgRed,
-    focused: crayon.bgLightRed,
-    active: crayon.bgYellow,
-    disabled: crayon.bgLightBlack,
-  },
-  zIndex: 1,
-});
-
-const button = new Button({
-  parent: tui,
-  rectangle: {
-    column: 50,
-    row: 5,
     width: 10,
-    height: 5,
   },
-  theme: {
-    base: crayon.bgRed,
-    focused: crayon.bgLightRed,
-    active: crayon.bgYellow,
-    disabled: crayon.bgLightBlack,
-  },
-  label: {
-    value: "hello\nworld\nthis is\nbutton\ntoo long line",
-  },
-  zIndex: 11,
 });
 
 new Button({
   parent: tui,
+  theme: baseTheme,
   rectangle: {
-    column: 62,
-    row: 5,
-    width: 10,
+    column: 15,
+    row: 3,
     height: 5,
-  },
-  theme: {
-    base: crayon.bgBlue,
-    focused: crayon.bgLightBlue,
-    active: crayon.bgCyan,
-    disabled: crayon.bgLightBlack,
-  },
-  label: {
-    value: "to the\nbottom\nright",
-    align: {
-      horizontal: "right",
-      vertical: "bottom",
-    },
-  },
-  zIndex: 2,
-});
-
-new Button({
-  parent: tui,
-  rectangle: {
-    column: 62,
-    row: 11,
     width: 10,
-    height: 5,
   },
-  theme: {
-    base: crayon.bgGreen,
-    focused: crayon.bgLightGreen,
-    active: crayon.bgLightYellow,
-    disabled: crayon.bgLightBlack,
-  },
-  label: {
-    value: "to the\ncenter\nleft",
-    align: {
-      vertical: "center",
-      horizontal: "left",
-    },
-  },
-  zIndex: 3,
 });
 
-const fpsCounter = new Text({
+new CheckBox({
   parent: tui,
+  theme: baseTheme,
   rectangle: {
-    column: 0,
-    row: 0,
-  },
-  theme: {
-    base: crayon.bgBlack.green,
-    focused: crayon.bgBlack.green,
-    active: crayon.bgBlack.green,
-    disabled: crayon.bgBlack.green,
-  },
-  value: "0 FPS",
-  zIndex: 0,
-});
-
-const text = new Text({
-  parent: tui,
-  rectangle: {
-    column: 2,
-    row: 9,
-  },
-  theme: {
-    base: crayon.bgBlack.red,
-    focused: crayon.bgBlack.lightRed,
-    active: crayon.bgBlack.yellow,
-    disabled: crayon.bgBlack.lightBlack,
-  },
-  value: "hello",
-  zIndex: 0,
-});
-
-const label = new Label({
-  parent: tui,
-  rectangle: {
-    column: 20,
-    row: 5,
-  },
-  theme: {
-    base: crayon.bgBlack.lightBlue,
-    focused: crayon.bgBlack.yellow,
-    active: crayon.lightYellow,
-    disabled: crayon.bgLightBlack,
-  },
-  value: `hello\nworld!\nhow are you?\n`,
-});
-
-const checkbox = new CheckBox({
-  parent: tui,
-  value: false,
-  rectangle: {
-    column: 20,
-    row: 1,
-    width: 1,
+    column: 28,
+    row: 3,
     height: 1,
+    width: 1,
   },
-  theme: {
-    base: crayon.bgMagenta.white,
-    focused: crayon.bgBlue.white,
-    active: crayon.bgGreen.white,
-    disabled: crayon.bgLightBlack.white,
-  },
+  value: false,
 });
 
 new ComboBox({
   parent: tui,
-  items: ["uno", "dos", "tres", "quatro", "cinque", "otto"],
+  theme: baseTheme,
   rectangle: {
-    column: 35,
-    row: 1,
+    column: 38,
+    row: 3,
     height: 1,
-    width: 10,
+    width: 7,
   },
-  theme: {
-    base: crayon.bgMagenta.white,
-    focused: crayon.bgBlue.white,
-    active: crayon.bgGreen.white,
-    disabled: crayon.bgLightBlack.white,
+  items: ["one", "two", "three", "four"],
+  zIndex: 2,
+});
+
+new ComboBox({
+  parent: tui,
+  theme: baseTheme,
+  rectangle: {
+    column: 38,
+    row: 7,
+    height: 1,
+    width: 7,
   },
-  zIndex: 5,
+  items: ["one", "two", "three", "four"],
+  label: {
+    value: "numer",
+  },
+  zIndex: 1,
 });
 
 const progressBar1 = new ProgressBar({
   parent: tui,
   orientation: "horizontal",
   direction: "normal",
-  rectangle: {
-    column: 74,
-    row: 1,
-    height: 2,
-    width: 10,
-  },
-  smooth: false,
-  min: 0,
-  max: 1,
-  value: 0.5,
   theme: {
-    base: crayon.bgYellow.green,
+    ...baseTheme,
     progress: {
-      base: crayon.bgGreen.yellow,
+      base: crayon.bgLightBlue.green,
+      focused: crayon.bgCyan.lightGreen,
+      active: crayon.bgBlue.lightYellow,
     },
   },
+  value: 50,
+  min: 0,
+  max: 100,
+  smooth: true,
+  rectangle: {
+    column: 48,
+    height: 2,
+    row: 3,
+    width: 10,
+  },
+});
+
+new Label({
+  parent: tui,
+  align: {
+    horizontal: "center",
+    vertical: "center",
+  },
+  rectangle: {
+    column: 75,
+    row: 3,
+  },
+  theme: { base: tui.style },
+  value: "Centered text\nThat automatically adjusts its rectangle size\n!@#!\nSo cool\nWOW",
 });
 
 const progressBar2 = new ProgressBar({
   parent: tui,
-  orientation: "horizontal",
-  direction: "reversed",
-  rectangle: {
-    column: 74,
-    row: 4,
-    height: 2,
-    width: 10,
-  },
-  smooth: false,
-  min: 0,
-  max: 1,
-  value: 0.5,
-  theme: {
-    base: crayon.bgYellow.green,
-    progress: {
-      base: crayon.bgGreen.yellow,
-    },
-  },
-});
-
-const progressBar3 = new ProgressBar({
-  parent: tui,
-  orientation: "horizontal",
+  orientation: "vertical",
   direction: "normal",
-  rectangle: {
-    column: 74,
-    row: 7,
-    height: 2,
-    width: 10,
-  },
-  smooth: true,
-  min: 0,
-  max: 1,
-  value: 0.5,
   theme: {
-    base: crayon.bgYellow.green,
+    ...baseTheme,
     progress: {
-      base: crayon.bgYellow.green,
+      base: crayon.bgLightBlue.green,
+      focused: crayon.bgCyan.lightGreen,
+      active: crayon.bgBlue.lightYellow,
     },
   },
-});
-
-const progressBar4 = new ProgressBar({
-  parent: tui,
-  orientation: "horizontal",
-  direction: "reversed",
+  value: 75,
+  min: 0,
+  max: 100,
+  smooth: true,
   rectangle: {
-    column: 74,
+    column: 48,
+    height: 5,
     row: 10,
-    height: 2,
-    width: 10,
-  },
-  smooth: true,
-  min: 0,
-  max: 1,
-  value: 0.5,
-  theme: {
-    base: crayon.bgYellow.green,
-    progress: {
-      base: crayon.bgYellow.green,
-    },
-  },
-});
-
-const progressBar5 = new ProgressBar({
-  parent: tui,
-  orientation: "vertical",
-  direction: "normal",
-  rectangle: {
-    column: 86,
-    row: 1,
-    height: 8,
-    width: 3,
-  },
-  smooth: false,
-  min: 0,
-  max: 1,
-  value: 0.5,
-  theme: {
-    base: crayon.bgYellow.green,
-    progress: {
-      base: crayon.bgGreen.yellow,
-    },
-  },
-});
-
-const progressBar6 = new ProgressBar({
-  parent: tui,
-  orientation: "vertical",
-  direction: "reversed",
-  rectangle: {
-    column: 91,
-    row: 1,
-    height: 8,
-    width: 3,
-  },
-  smooth: false,
-  min: 0,
-  max: 1,
-  value: 0.5,
-  theme: {
-    base: crayon.bgYellow.green,
-    progress: {
-      base: crayon.bgGreen.yellow,
-    },
-  },
-});
-
-const progressBar7 = new ProgressBar({
-  parent: tui,
-  orientation: "vertical",
-  direction: "normal",
-  rectangle: {
-    column: 96,
-    row: 1,
-    height: 8,
-    width: 3,
-  },
-  smooth: true,
-  min: 0,
-  max: 1,
-  value: 0.5,
-  theme: {
-    base: crayon.bgYellow.green,
-    progress: {
-      base: crayon.bgYellow.green,
-    },
-  },
-});
-
-const progressBar8 = new ProgressBar({
-  parent: tui,
-  orientation: "vertical",
-  direction: "reversed",
-  rectangle: {
-    column: 101,
-    row: 1,
-    height: 8,
-    width: 3,
-  },
-  smooth: true,
-  min: 0,
-  max: 1,
-  value: 0.5,
-  theme: {
-    base: crayon.bgYellow.green,
-    progress: {
-      base: crayon.bgYellow.green,
-    },
+    width: 2,
   },
 });
 
 new Slider({
   parent: tui,
   orientation: "horizontal",
+  theme: {
+    ...baseTheme,
+    thumb: {
+      base: crayon.bgMagenta,
+    },
+  },
+  value: 5,
+  min: 1,
+  max: 10,
+  step: 1,
+  rectangle: {
+    column: 61,
+    height: 2,
+    row: 3,
+    width: 10,
+  },
+});
+
+new Slider({
+  parent: tui,
+  orientation: "vertical",
+  theme: {
+    ...baseTheme,
+    thumb: {
+      base: crayon.bgMagenta,
+    },
+  },
+  value: 5,
+  min: 1,
+  max: 10,
+  step: 1,
+  rectangle: {
+    column: 61,
+    height: 5,
+    row: 10,
+    width: 2,
+  },
+});
+
+new Input({
+  parent: tui,
+  placeholder: "type smth",
+  theme: {
+    ...baseTheme,
+    cursor: {
+      base: crayon.invert,
+    },
+    value: {
+      base: crayon.bgBlue.white,
+    },
+    placeholder: {
+      base: crayon.bgLightBlue.lightBlack,
+      focused: crayon.bgCyan.lightBlack,
+      active: crayon.bgBlue.lightBlack,
+    },
+  },
   rectangle: {
     column: 2,
-    row: 10,
-    height: 2,
-    width: 12,
-  },
-  smooth: true,
-  min: 0,
-  max: 1,
-  step: 1 / 11,
-  value: 0.5,
-  theme: {
-    base: crayon.bgYellow.green,
-    focused: crayon.bgMagenta.green,
-    thumb: {
-      base: crayon.bgBlue.green,
-    },
+    row: 11,
+    width: 10,
+    height: 1,
   },
 });
 
-new Slider({
+/* new TextboxComponent({
+  tui,
+  theme: baseTheme,
+  multiline: false,
+  rectangle: {
+    column: 2,
+    row: 11,
+    height: 1,
+    width: 10,
+  },
+  value: "hi",
+});
+
+new TextboxComponent({
+  tui,
+  theme: {
+    ...baseTheme,
+    placeholder: crayon.lightBlack,
+  },
+  multiline: false,
+  rectangle: {
+    column: 2,
+    row: 15,
+    height: 1,
+    width: 10,
+  },
+  placeholder: "example",
+});
+
+new TextboxComponent({
+  tui,
+  theme: baseTheme,
+  multiline: false,
+  hidden: true,
+  rectangle: {
+    column: 15,
+    row: 11,
+    height: 1,
+    width: 10,
+  },
+  value: "hi!",
+});
+
+new TextboxComponent({
+  tui,
+  theme: {
+    ...baseTheme,
+    lineNumbers: {
+      base: crayon.bgBlue.white,
+    },
+    highlightedLine: {
+      base: crayon.bgLightBlue,
+    },
+  },
+  multiline: true,
+  lineNumbering: true,
+  lineHighlighting: true,
+  hidden: false,
+  rectangle: {
+    column: 29,
+    row: 11,
+    height: 5,
+    width: 12,
+  },
+  value: "hello!\nwhats up?",
+}); */
+
+new Table({
   parent: tui,
+  theme: {
+    base: crayon.bgBlack.white,
+    frame: { base: crayon.bgBlack },
+    header: { base: crayon.bgBlack.bold.lightBlue },
+    selectedRow: {
+      base: crayon.bold.bgBlue.white,
+      focused: crayon.bold.bgLightBlue.white,
+      active: crayon.bold.bgMagenta.black,
+    },
+  },
+  rectangle: {
+    column: 75,
+    height: 8,
+    row: 11,
+  },
+  headers: [
+    { title: "ID" },
+    { title: "Name" },
+  ],
+  data: [
+    ["0", "Thomas Jeronimo"],
+    ["1", "Jeremy Wanker"],
+    ["2", "Julianne James"],
+    ["3", "Tommie Moyer"],
+    ["4", "Marta Reilly"],
+    ["5", "Bernardo Robertson"],
+    ["6", "Hershel Grant"],
+  ],
+  charMap: "rounded",
+});
+
+const view = new View({
+  rectangle: {
+    column: 125,
+    row: 1,
+    width: 10,
+    height: 10,
+  },
+  maxOffset: {
+    columns: 0,
+    rows: 20,
+  },
+});
+
+const viewBackground = new Box({
+  parent: tui,
+  rectangle: {
+    column: view.rectangle.column,
+    row: view.rectangle.row,
+    width: view.rectangle.width,
+    height: view.rectangle.height,
+  },
+  theme: {
+    base: crayon.bgLightBlack,
+  },
+  zIndex: 1,
+});
+// @ts-ignore-
+viewBackground.NOFRAME = true;
+
+const viewScrollbar = new Slider({
+  parent: tui,
+  min: 0,
+  max: view.maxOffset.rows,
+  value: 0,
+  step: 1,
   orientation: "vertical",
+  adjustThumbSize: true,
+  rectangle: {
+    column: view.rectangle.column + view.rectangle.width - 1,
+    row: view.rectangle.row,
+    height: view.rectangle.height,
+    width: 1,
+  },
+  theme: {
+    thumb: { base: crayon.bgRed },
+    base: crayon.bgLightBlue,
+  },
+  zIndex: 2,
+});
+// @ts-ignore-
+viewScrollbar.NOFRAME = true;
+
+viewScrollbar.on("mousePress", ({ scroll }) => {
+  if (!scroll) return;
+  viewScrollbar.value = clamp(viewScrollbar.value + scroll * viewScrollbar.step, viewScrollbar.min, viewScrollbar.max);
+  viewScrollbar.emit("valueChange", viewScrollbar);
+});
+
+viewScrollbar.on("valueChange", () => {
+  view.offset.rows = viewScrollbar.value;
+  viewBackground.rectangle.row = view.rectangle.row;
+  viewScrollbar.rectangle.row = view.rectangle.row;
+});
+
+const box = new Box({
+  parent: tui,
+  view,
+  rectangle: {
+    column: 2,
+    row: 1,
+    width: 4,
+    height: 2,
+  },
+  theme: {
+    base: crayon.bgRed,
+  },
+  zIndex: 2,
+});
+
+box.interact = () => {
+  box.state = "focused";
+};
+
+box.on("mousePress", ({ drag, movementX, movementY }) => {
+  if (!drag) return;
+  box.rectangle.column += movementX;
+  box.rectangle.row += movementY;
+});
+
+new Button({
+  parent: tui,
+  view,
+  rectangle: {
+    column: 2,
+    row: 9,
+    width: 4,
+    height: 2,
+  },
+  theme: {
+    base: crayon.bgGreen,
+    focused: crayon.bgLightGreen,
+    active: crayon.bgMagenta,
+  },
+  zIndex: 2,
+});
+
+new Text({
+  parent: tui,
+  view,
   rectangle: {
     column: 2,
     row: 13,
-    height: 8,
-    width: 2,
   },
-  smooth: true,
-  min: 0,
-  max: 1,
-  step: 0.1,
-  value: 0.5,
-  theme: {
-    base: crayon.bgYellow.green,
-    focused: crayon.bgMagenta.green,
-    thumb: {
-      base: crayon.bgBlue.green,
-    },
-  },
+  theme: baseTheme,
+  value: "wopa",
+  zIndex: 2,
 });
 
-new Frame({
-  parent: tui,
-  rectangle: {
-    column: 10,
-    row: 12,
-    width: 10,
-    height: 5,
-  },
-  theme: {
-    base: crayon.bgBlack.bold.white,
-  },
-  charMap: "rounded",
+// Generate frames and labels for every component
+queueMicrotask(() => {
+  const components: Component[] = [];
+
+  for (const component of tui.components) {
+    // @ts-ignore-
+    if (component.view || component.NOFRAME || component === performanceStats) continue;
+
+    const name = component.constructor.name;
+
+    const { column, row } = component.rectangle;
+    components.push(
+      new Text({
+        parent: tui,
+        theme: { base: tui.style },
+        rectangle: {
+          column: column - 1,
+          row: row - 2,
+        },
+        value: name,
+        zIndex: component.zIndex,
+      }),
+      new Frame({
+        parent: tui,
+        component,
+        charMap: "rounded",
+        theme: { base: tui.style },
+        zIndex: component.zIndex,
+      }),
+    );
+  }
+
+  tui.on("keyPress", ({ ctrl, meta, shift, key }) => {
+    console.log(ctrl, meta, shift, key);
+
+    if (!ctrl || key !== "f" || meta || shift) return;
+    for (const component of components) {
+      component.visible = !component.visible;
+    }
+  });
 });
 
-new Frame({
-  parent: tui,
-  component: checkbox,
-  theme: {
-    base: crayon.bgBlack.bold.white,
-  },
-  charMap: "rounded",
-});
-
-new Input({
-  parent: tui,
-  rectangle: {
-    column: 60,
-    row: 1,
-    width: 10,
-    height: 1,
-  },
-  value: "Hello",
-  theme: {
-    base: crayon.bgYellow.green,
-    focused: crayon.bgMagenta.green,
-    value: {
-      base: crayon.bgGreen.red,
-      focused: crayon.bgCyan.magenta,
-      active: crayon.bgRed,
-    },
-    cursor: {
-      base: crayon.bgWhite.black.bold,
-    },
-  },
-});
-
-new Input({
-  parent: tui,
-  rectangle: {
-    column: 60,
-    row: 3,
-
-    width: 10,
-    height: 1,
-  },
-  value: "",
-  theme: {
-    base: crayon.bgYellow.green,
-    focused: crayon.bgMagenta.green,
-    value: {
-      base: crayon.bgGreen.red,
-      focused: crayon.bgCyan.magenta,
-      active: crayon.bgRed,
-    },
-    cursor: {
-      base: crayon.bgWhite.black.bold,
-    },
-  },
-  validator: /\d/,
-});
-
-button.on("mousePress", ({ drag, movementX, movementY }) => {
-  if (!drag) return;
-
-  button.rectangle.column += movementX;
-  button.rectangle.row += movementY;
-});
-
+let fps = 60;
+let lastRender = 0;
+let progressBarDir = 1;
 tui.canvas.on("render", () => {
-  fpsCounter.value = `${tui.canvas.fps.toFixed(2)} FPS`;
-  text.value = `hello ${(Math.random() * 100).toFixed(5)}`;
+  fps = 1000 / (performance.now() - lastRender);
+  lastRender = performance.now();
+
+  progressBar1.value = progressBar2.value += progressBarDir;
+  if (progressBar1.value >= progressBar1.max || progressBar1.value <= progressBar1.min) {
+    progressBarDir *= -1;
+  }
+
+  performanceStats.value = `\
+FPS: ${fps.toFixed(2)}\
+ | Components: ${tui.components.length}\
+ | Drawn objects: ${tui.canvas.drawnObjects.length}\
+ | Press CTRL+F to toggle Frame/Label visibility`;
 });
-
-let i = 0;
-let dir = 0.025;
-
-setInterval(() => {
-  label.value = `hello\nworld!\nhow are you?\n${`${i % 5} lines\n`.repeat(i++ % 5)}`;
-  text.rectangle.column++;
-
-  progressBar8.value += dir;
-
-  progressBar1.value =
-    progressBar2.value =
-    progressBar3.value =
-    progressBar4.value =
-    progressBar5.value =
-    progressBar6.value =
-    progressBar7.value =
-      progressBar8.value;
-
-  if (progressBar1.value >= 1 || progressBar1.value <= 0) dir *= -1;
-}, 80);
