db20032c2f2f2d16062441dfb0d57519d19faa7e
Im-Beast <franik.mateusz@gmail.com>
2023-05-04 01:37:30 +0200

    feat: adapt numbers utils to use signals

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -1,4 +1,4 @@
-// Copyright 2022 Im-Beast. All rights reserved. MIT license.
+// Copyright 2023 Im-Beast. All rights reserved. MIT license.
 
 import type { Rectangle } from "../types.ts";
 
@@ -26,35 +26,38 @@ export function rectangleEquals(a: Rectangle, b: Rectangle): boolean {
   return (a === b) || (a.column === b.column && a.row === b.row && a.width === b.width && a.height === b.height);
 }
 
+const intersectionObject = { column: 0, row: 0, width: 0, height: 0 };
 export function rectangleIntersection(a: Rectangle, b: Rectangle, data?: false): boolean;
 export function rectangleIntersection(a: Rectangle, b: Rectangle, data: true): false | Rectangle;
 export function rectangleIntersection(a: Rectangle, b: Rectangle, data?: boolean): boolean | Rectangle {
   const { column: c1, row: r1, width: w1, height: h1 } = a;
-  if (w1 === 0) false;
-  const { column: c2, height: h2, width: w2, row: r2 } = b;
-  if (w2 === 0) false;
+  if (w1 === 0 || h1 === 0) false;
+  const { column: c2, row: r2, width: w2, height: h2 } = b;
+  if (w2 === 0 || h2 === 0) false;
 
   if (
-    c1 >= c2 + w2 ||
-    c2 >= c1 + w1 ||
-    r1 >= r2 + h2 ||
-    r2 >= r1 + h1
-  ) return false;
-
-  if (!data) return true;
+    c1 >= c2 + w2 || c2 >= c1 + w1 ||
+    r1 >= r2 + h2 || r2 >= r1 + h1
+  ) {
+    return false;
+  } else if (!data) {
+    return true;
+  }
 
   const colWidth = Math.min(c1 + w1, c2 + w2);
   const rowHeight = Math.min(r1 + h1, r2 + h2);
 
   const width = Math.max(0, colWidth - Math.max(c1, c2));
+  if (width === 0) return false;
   const height = Math.max(0, rowHeight - Math.max(r1, r2));
+  if (height === 0) return false;
 
-  if (width === 0 || height === 0) return false;
+  intersectionObject.column = colWidth - width;
+  intersectionObject.row = rowHeight - height;
+  intersectionObject.width = width;
+  intersectionObject.height = height;
 
-  const column = colWidth - width;
-  const row = rowHeight - height;
-
-  return { column, row, width, height };
+  return intersectionObject;
 }
 
 /** Normalize {value} between 0 and 1 */
