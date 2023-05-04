a6407d72b8b47e2faa6624eed82bb4011630fb49
Im-Beast <franik.mateusz@gmail.com>
2023-05-04 13:26:25 +0200

    feat: remove unused deferred module

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,7 +1,6 @@
 // Copyright 2023 Im-Beast. All rights reserved. MIT license.
 export * from "./ansi_codes.ts";
 export * from "./async.ts";
-export * from "./deffered.ts";
 export * from "./numbers.ts";
 export * from "./sorted_array.ts";
 export * from "./strings.ts";
