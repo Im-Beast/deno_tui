cabf6ea1c6a6e2dfcc4e4382ed1da6e18177c102
Im-Beast <franik.mateusz@gmail.com>
2023-05-30 17:38:47 +0200

    fix: re-export all modules in mod.ts

--- src/signals/mod.ts
+++ src/signals/mod.ts
@@ -1,5 +1,7 @@
 // Copyright 2023 Im-Beast. All rights reserved. MIT license.
 export * from "./signal.ts";
-export * from "./signal.ts";
 export * from "./computed.ts";
 export * from "./effect.ts";
+export * from "./dependency_tracking.ts";
+export * from "./reactivity.ts";
+export * from "./types.ts";
