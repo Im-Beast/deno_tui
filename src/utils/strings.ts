2055f4b83c8f45b24db8251e3feca96b28b4964d
Im-Beast <franik.mateusz@gmail.com>
2023-07-29 20:41:24 +0200

    perf: highly optimize textWidth, stripStyles and cropToWidth functions

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -8,12 +8,23 @@
 export const UNICODE_CHAR_REGEXP =
   /\ud83c[\udffb-\udfff](?=\ud83c[\udffb-\udfff])|(?:(?:\ud83c\udff4\udb40\udc67\udb40\udc62\udb40(?:\udc65|\udc73|\udc77)\udb40(?:\udc6e|\udc63|\udc6c)\udb40(?:\udc67|\udc74|\udc73)\udb40\udc7f)|[^\ud800-\udfff][\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff\u1ab0-\u1aff\u1dc0-\u1dff]?|[\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff\u1ab0-\u1aff\u1dc0-\u1dff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\ud800-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff\u1ab0-\u1aff\u1dc0-\u1dff]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe2f\u20d0-\u20ff\u1ab0-\u1aff\u1dc0-\u1dff]|\ud83c[\udffb-\udfff])?)*/g;
 
-// deno-lint-ignore no-control-regex
-const STRIP_STYLE_REGEXP = /\x1b\[([0-9]|;)+m/gi;
-
 /** Strips string of all its styles */
 export function stripStyles(string: string): string {
-  return string.replaceAll(STRIP_STYLE_REGEXP, "");
+  let stripped = "";
+  let ansi = false;
+  const len = string.length;
+  for (let i = 0; i < len; ++i) {
+    const char = string[i];
+    if (char === "\x1b") {
+      ansi = true;
+      i += 2; // [ "\x1b" "[" "X" "m" ] <-- shortest ansi sequence
+    } else if (char === "m" && ansi) {
+      ansi = false;
+    } else if (!ansi) {
+      stripped += char;
+    }
+  }
+  return stripped;
 }
 
 /** Inserts {value} into {string} on given {index} */
@@ -22,25 +33,45 @@ export function insertAt(string: string, index: number, value: string): string {
 }
 
 /** Returns real {text} width */
-export function textWidth(text: string): number {
+export function textWidth(text: string, start = 0): number {
   if (!text) return 0;
 
   let width = 0;
-  const strippedString = stripStyles(text);
-  for (let i = 0; i < strippedString.length; i++) {
-    width += characterWidth(strippedString[i]);
+  let ansi = false;
+  const len = text.length;
+  for (let i = start; i < len; ++i) {
+    const char = text[i];
+    if (char === "\x1b") {
+      ansi = true;
+      i += 2; // [ "\x1b" "[" "X" "m" ] <-- shortest ansi sequence
+    } else if (char === "m" && ansi) {
+      ansi = false;
+    } else if (!ansi) {
+      width += characterWidth(char);
+    }
   }
 
   return width;
 }
-
 /** Crops {text} to given {width} */
 export function cropToWidth(text: string, width: number): string {
-  text = text.slice(0, width);
+  const stripped = stripStyles(text);
+  const letter = stripped[width];
+
   if (textWidth(text) <= width) return text;
+
+  text = text.slice(0, text.lastIndexOf(letter));
+  if (textWidth(text) <= width) return text;
+
+  const start = text.indexOf(letter);
+  const knownPart = text.slice(0, start);
+  const knownWidth = textWidth(knownPart);
+  if (knownWidth === width) return knownPart;
+
   do {
-    text = text.slice(0, -1);
-  } while (textWidth(text) > width);
+    const index = text.lastIndexOf(letter);
+    text = text.slice(0, index);
+  } while ((knownWidth + textWidth(text, start)) > width);
   return text;
 }
 
